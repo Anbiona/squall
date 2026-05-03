9afeb610f881a5e3d1873506db3f073647bb8c10
Anbiona <anbiona@tutanota.com>
2026-05-03 12:54:55 -0500

    feat: add user creation services and navidrome user

---
+++ files/system/usr/libexec/user-setup.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+ 
+while IFS=: read -r username _ uid gid _ homedir shell; do
+    [[ "$username" == "root" ]] && continue
+    [[ "$shell" == */false ]]   && continue
+ 
+    if [[ "$homedir" == "/var/lib/${username}" ]]; then
+        if [[ ! -d "$homedir" ]]; then
+            install -d -m 0750 -o "$uid" -g "$gid" "$homedir"
+            install -d -m 0750 -o "$uid" -g "$gid" "$homedir/data"
+            touch /var/lib/systemd/linger/"$username"
+
+            case "$username" in
+            navidrome)
+                install -d -m 0750 -o "$uid" -g "$gid" "$homedir/music"
+                ;;
+            esac
+        fi
+    fi
+done < /etc/passwd
